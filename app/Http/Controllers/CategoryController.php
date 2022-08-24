4f229d9a6f28cf903a3b3351d91d85dd0b6e8518
agung1997-mhs <agungkemayoran97@gmail.com>
2022-08-24 11:57:08 +0700

    Navbar Update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('pages.category');
+        $categories = Category::all();
+        $products = Product::with(['galleries'])->paginate(3);
+
+        return view('pages.category', [
+            'categories' => $categories,
+            'products' => $products
+        ]);
+    }
+
+    public function detail(Request $request, $slug)
+    {
+        $categories = Category::all();
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(3);
+
+        return view('pages.category', [
+            'categories' => $categories,
+            'products' => $products
+        ]);
     }
 }
--- resources/views/includes/navbar-authenticated.blade.php
+++ resources/views/includes/navbar-authenticated.blade.php
@@ -28,45 +28,45 @@
         </ul>
         <!-- Desktop Menu -->
         <ul class="navbar-nav d-none d-lg-flex">
-        <li class="nav-item dropdown">
-            <a
-            href="#"
-            class="nav-link"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            >
-            <img
-                src="/images/ic_user.png"
-                alt=""
-                class="rounded-circle mr-2 profile-picture"
-            />
-            Hi, Agung
-            </a>
-            <div class="dropdown-menu">
-            <a href="/dashboard.html" class="dropdown-item">Dashboard</a>
-            <a href="/dashboard-account.html" class="dropdown-item"
-                >Settings</a
-            >
-            <div class="dropdown-divider"></div>
-            <a href="/" class="dropdown-item">Logout</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link d-inline-block mt-2">
-            <img src="/images/ic_cart_empty.svg" alt="" />
-            </a>
-        </li>
+            <li class="nav-item dropdown">
+                <a
+                href="#"
+                class="nav-link"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                >
+                <img
+                    src="/images/ic_user.png"
+                    alt=""
+                    class="rounded-circle mr-2 profile-picture"
+                />
+                Hi, Agung
+                </a>
+                <div class="dropdown-menu">
+                <a href="/dashboard.html" class="dropdown-item">Dashboard</a>
+                <a href="/dashboard-account.html" class="dropdown-item"
+                    >Settings</a
+                >
+                <div class="dropdown-divider"></div>
+                <a href="/" class="dropdown-item">Logout</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link d-inline-block mt-2">
+                <img src="/images/ic_cart_empty.svg" alt="" />
+                </a>
+            </li>
         </ul>
 
         <!-- Mobile Menu -->
         <ul class="navbar-nav d-block d-lg-none">
-        <li class="nav-item">
-            <a href="#" class="nav-link"> Hi, Agung </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link d-inline-block"> Cart </a>
-        </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Hi, Agung </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link d-inline-block"> Cart </a>
+            </li>
         </ul>
     </div>
     </div>
