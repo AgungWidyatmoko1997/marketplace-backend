fcd11f9c879101d9823b62e9ead7a143b3fdd80d
agung1997-mhs <agungkemayoran97@gmail.com>
2022-08-28 20:26:36 +0700

    menambahkan data produk ke dalam cart

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart;
+use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DetailController extends Controller
 {
-    public function index()
+    public function index(Request $request, $id)
     {
-        return view('pages.detail');
+        $product = Product::with(['galleries','user'])->where('slug', $id)->firstOrFail();
+
+        return view('pages.detail', [
+            'product' => $product
+        ]);
+    }
+
+    public function add(Request $request, $id)
+    {
+        $data = [
+            'products_id' => $id,
+            'users_id' => Auth::user()->id
+        ];
+
+        Cart::create($data);
+
+        return redirect()->route('cart');
+        
     }
 }
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -15,7 +15,7 @@
               <nav>
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item">
-                    <a href="/index.html">Home</a>
+                    <a href="{{ route('home') }}">Home</a>
                   </li>
                   <li class="breadcrumb-item active">Cart</li>
                 </ol>
@@ -39,66 +39,36 @@
                   </tr>
                 </thead>
                 <tbody>
+                  @foreach ($carts as $cart)
                   <tr>
                     <td style="width: 20%">
-                      <img
-                        src="/images/product-cart-1.jpg"
-                        alt=""
-                        class="cart-image"
-                      />
+                        @if($cart->product->galleries)
+                        <img
+                          src="{{ Storage::url($cart->product->galleries->first()->photos) }}"
+                          alt=""
+                          class="cart-image"
+                        />
+                      @endif
                     </td>
                     <td style="width: 35%">
-                      <div class="product-title">Sofa Ternyaman</div>
-                      <div class="product-subtitle">by Andi Sukka</div>
+                      <div class="product-title">{{ $cart->product->name }}</div>
+                      <div class="product-subtitle">by {{ $cart->user->name }}</div>
                     </td>
                     <td style="width: 35%">
-                      <div class="product-title">$29,112</div>
-                      <div class="product-subtitle">USD</div>
+                      <div class="product-title">Rp. {{ number_format($cart->product->price) }}</div>
+                      <div class="product-subtitle">Rupiah</div>
                     </td>
                     <td style="width: 20%">
-                      <a href="#" class="btn btn-remove-cart">Remove</a>
+                     <form action="{{ route('cart-delete', $cart->id) }}" method="POST">
+                      @method('DELETE')
+                      @csrf
+                      <button type="submit" class="btn btn-remove-cart">
+                        Remove
+                      </button>
+                     </form>
                     </td>
                   </tr>
-                  <tr>
-                    <td style="width: 20%">
-                      <img
-                        src="/images/product-cart-2.jpg"
-                        alt=""
-                        class="cart-image"
-                      />
-                    </td>
-                    <td style="width: 35%">
-                      <div class="product-title">Sneaker</div>
-                      <div class="product-subtitle">by BuildWith Angga</div>
-                    </td>
-                    <td style="width: 35%">
-                      <div class="product-title">$80,309</div>
-                      <div class="product-subtitle">USD</div>
-                    </td>
-                    <td style="width: 20%">
-                      <a href="#" class="btn btn-remove-cart">Remove</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="width: 20%">
-                      <img
-                        src="/images/product-cart-3.jpg"
-                        alt=""
-                        class="cart-image"
-                      />
-                    </td>
-                    <td style="width: 35%">
-                      <div class="product-title">Coffee Holder</div>
-                      <div class="product-subtitle">by Addictex</div>
-                    </td>
-                    <td style="width: 35%">
-                      <div class="product-title">$13,4922</div>
-                      <div class="product-subtitle">USD</div>
-                    </td>
-                    <td style="width: 20%">
-                      <a href="#" class="btn btn-remove-cart">Remove</a>
-                    </td>
-                  </tr>
+                  @endforeach
                 </tbody>
               </table>
             </div>
